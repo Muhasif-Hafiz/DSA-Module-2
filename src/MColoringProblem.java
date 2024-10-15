0095a0a81fb085110e34438df933f08084532cd8
Muhasif Hafiz <muhasibhafeez91@gmail.com>
2024-10-15 20:40:14 +0530

    Sudoku Solver Leetcode Hard 37

---
+++ src/MColoringProblem.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class MColoringProblem {
+    public static void main(String[] args) {
+
+    }
+  public static   boolean graphColoring(int v, List<int[]> edges, int m) {
+        int n=edges.size();
+
+        boolean ans=solve(v, edges, m, n);
+        return ans;
+    }
+    public static boolean  solve(int node, int v, List<int [] > edges, int m, int n){
+
+        if(node==n) return true;
+
+        //incomplete ,,,,, Finish After Graph Series
+        return true;
+    }
+}
